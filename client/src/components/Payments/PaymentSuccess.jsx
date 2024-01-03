415db659ed214144a86154eef0cd779fd519ea4c
Jai <kushwahajai74@gmail.com>
2024-01-03 20:16:51 +0530

    fixed issue with payment integration

--- client/src/components/Payments/PaymentSuccess.jsx
+++ client/src/components/Payments/PaymentSuccess.jsx
@@ -1,17 +1,18 @@
 import {
   Box,
   Button,
-  Center,
   Container,
   Heading,
   Text,
   VStack,
+  useQuery,
 } from "@chakra-ui/react";
 import React from "react";
 import { RiCheckboxCircleFill } from "react-icons/ri";
-import { Link } from "react-router-dom";
+import { Link, useSearchParams } from "react-router-dom";
 
 const PaymentSuccess = () => {
+  const reference = useSearchParams()[0].get("reference");
   return (
     <Container h={"90vh"} p={16}>
       <Heading children="You Have Pro Pack" my={8} textAlign={"center"} />
@@ -40,7 +41,7 @@ const PaymentSuccess = () => {
         <Link to={"/profile"}>
           <Button children="Go to Profile" colorScheme="yellow" />
         </Link>
-        <Heading size={"sm"} children="Refrence: dfjdkfhksdjfjdfjsdfh" />
+        <Heading size={"sm"} children={`Refrence: ${reference}`} />
       </VStack>
     </Container>
   );
